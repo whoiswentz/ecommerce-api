41fc0445b00ceb5b9dadb1089042bd1744054169
Vinicios Wentz <vinicios@wentz.io>
2023-09-30 11:50:05 -0300

    feat(:system_requirements): update controller method

--- app/controllers/admin/v1/system_requirements_controller.rb
+++ app/controllers/admin/v1/system_requirements_controller.rb
@@ -1,5 +1,7 @@
 module Admin::V1
   class SystemRequirementsController < ApiController
+    before_action :find_system_requirement, only: [:update]
+
     def index
       @system_requirements = SystemRequirement.all
     end
@@ -13,10 +15,22 @@ module Admin::V1
       end
     end
 
+    def update
+      if @system_requirement.update(system_requirement_params)
+        render :show
+      else
+        render_error(fields: @system_requirement.errors.messages)
+      end
+    end
+
     private
 
     def system_requirement_params
       params.require(:system_requirement).permit(:name, :os, :storage, :cpu, :memory, :gpu)
+    end
+
+    def find_system_requirement
+      @system_requirement = SystemRequirement.find(params[:id])
     end
   end
 end
--- spec/requests/admin/v1/system_requirement_spec.rb
+++ spec/requests/admin/v1/system_requirement_spec.rb
@@ -35,4 +35,52 @@ RSpec.describe Admin::V1::SystemRequirementsController, type: :request do
       it_behaves_like "simple post request with valid params", SystemRequirement, 'system_requirement', %i(name os storage cpu memory gpu)
     end
   end
+
+  context "PATCH /system_requirements/:id" do
+    let(:system_requirement) { create(:system_requirement) }
+    let(:url) { "/admin/v1/system_requirements/#{system_requirement.id}" }
+
+    context "with invalid params" do
+      let(:params) { { system_requirement: attributes_for(:system_requirement, name: nil) }.to_json }
+
+      it "should not update" do
+        old_name = system_requirement.name
+        patch url, headers: auth_header(user), params: params
+        system_requirement.reload
+        expect(system_requirement.name).to eq old_name
+      end
+
+      it "should return error message" do
+        patch url, headers: auth_header(user), params: params
+        expect(json_body['errors']['fields']).to have_key('name')
+      end
+
+      it "should return unprocessable entity status" do
+        patch url, headers: auth_header(user), params: params
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context "with valid params" do
+      let(:name) { "New name"  }
+      let(:params) { { system_requirement: attributes_for(:system_requirement, name: name) }.to_json }
+
+      it "should update" do
+        patch url, headers: auth_header(user), params: params
+        system_requirement.reload
+        expect(system_requirement.name).to eq name
+      end
+
+      it "should return updated json" do
+        patch url, headers: auth_header(user), params: params
+        system_requirement.reload
+        expect(json_body['system_requirement']).to eq system_requirement.as_json(only: %i(name os storage cpu memory gpu))
+      end
+
+      it "should return http status ok" do
+        patch url, headers: auth_header(user), params: params
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
 end
